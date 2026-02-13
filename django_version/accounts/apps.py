2632e4d7c46af9e57743616cfb6347cd44d6b2cf
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-13 09:22:53 +0100

    config: add accounts to settings installed_apps and update AccountsConfig to be receive ready()

--- django_version/accounts/apps.py
+++ django_version/accounts/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = "accounts"
+    verbose_name = "User Accounts"
+
+    def ready(self):
+        """
+        Executed when Django loads the app.
+        Perform initialization tasks such as registering signals, validators, etc.
+        It is called as soon as the registry is fully populated.
+        """
+        pass
