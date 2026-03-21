38411fdf0c9555d95b05f80969cdcb4f4eebcbac
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-21 19:35:31 +0100

    feat(accounts/models): Update init__.py with Freelancer, Client and BaseUser imports

---
+++ django_version/accounts/models/__init__.py
@@ -0,0 +1,5 @@
+from accounts.models.base import BaseUser, BaseUserManager
+from accounts.models.freelancer import Freelancer
+from accounts.models.client import Client
+
+__all__ = ["BaseUser", "BaseUserManager", "Freelancer", "Client"]
