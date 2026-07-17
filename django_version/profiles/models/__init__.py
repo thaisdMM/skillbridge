d417fdd63a8bf5efc04c241b932e507c6dee605b
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-07-17 16:09:45 +0200

    fix(profiles/models): update__init__.py  __all__ to include ClientProfile and reorder imports

--- django_version/profiles/models/__init__.py
+++ django_version/profiles/models/__init__.py
@@ -1,5 +1,6 @@
 from profiles.models.base import Profile
-from profiles.models.skill import Skill
+from profiles.models.client_profile import ClientProfile
 from profiles.models.freelancer_profile import FreelancerProfile
+from profiles.models.skill import Skill
 
-__all__ = ["Profile", "Skill", "FreelancerProfile"]
+__all__ = ["Profile", "ClientProfile", "FreelancerProfile", "Skill"]
